873d56db6b6a01e3f27c4ecfec43146ba0cb464b
hardik500 <hardikkhandelwal500@gmail.com>
2019-06-10 00:09:19 +0530

    Updates

---
+++ precache-manifest.d49ba363c5d0b42be51174c77b3e9f35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fded2ee4d73600827cbf133e26be03c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9a78db252ceffb509bd2",
+    "url": "/portfolio/static/css/main.3b3a3a3d.chunk.css"
+  },
+  {
+    "revision": "8364e01c2685ebbb9a9a",
+    "url": "/portfolio/static/js/2.9d401577.chunk.js"
+  },
+  {
+    "revision": "9a78db252ceffb509bd2",
+    "url": "/portfolio/static/js/main.81b13391.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
